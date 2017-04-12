ace9f0b598640b59372e81ebb9ef62cb93408245
alxngyn <alexlnguyen@gmail.com>
2017-04-11 18:59:01 -0700

    functionalize w

--- project1/main.py
+++ project1/main.py
@@ -2,21 +2,27 @@ import numpy as np
 
 def main():
     ### input
-    x = np.loadtxt(open("housing_train.txt", "rb"), delimiter=" ", usecols = range(12))
-    print x
-    y = np.loadtxt(open("housing_train.txt", "rb"), delimiter=" ", usecols = [13])
-    print y
+    x_train = np.loadtxt(open("housing_train.txt", "rb"), delimiter=" ", usecols = range(12))
+    y_train = np.loadtxt(open("housing_train.txt", "rb"), delimiter=" ", usecols = [13])
+    # x_test = np.loadtxt(open("housing_test.txt", "rb"), delimiter=" ", usecols = range(12))
+    # y_test = np.loadtxt(open("housing_test.txt", "rb"), delimiter=" ", usecols = [13])
 
+    print "Training W: "
+    print compute_w(x_train, y_train)
+
+    # print "Testing W: "
+    # print compute_w(x_test, y_test)
+
+
+
+def compute_w(x,y):
     ### compute optimal weight vector w
-
     xTx = np.matmul(np.transpose(x), x)
     xTx_inverse = np.linalg.inv(xTx)
     xTy = np.matmul(np.transpose(x), y)
 
     w = np.matmul(xTx_inverse, xTy)
-    print "W: "
-    print w
-
+    return w
 
 if __name__ == '__main__':
     main()
