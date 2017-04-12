33f4480dab9cd92fba509bec2d627bcb6af5a624
Brandon Lee <brandonlee503@gmail.com>
2017-04-11 17:54:32 -0700

    Part 1

--- project1/main.py
+++ project1/main.py
@@ -1,5 +1,10 @@
+import numpy
+
 def main():
-    
+    x = numpy.loadtxt(open("housing_train.txt", "rb"), delimiter=" ", usecols = range(12))
+    print x
+    y = numpy.loadtxt(open("housing_train.txt", "rb"), delimiter=" ", usecols = [13])
+    print y
 
 if __name__ == '__main__':
     main()
