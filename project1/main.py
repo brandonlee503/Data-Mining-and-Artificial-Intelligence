284020a20cd42d5d6287db81e4e12a5792c19444
alxngyn <alexlnguyen@gmail.com>
2017-04-11 18:12:46 -0700

    added w

--- project1/main.py
+++ project1/main.py
@@ -1,10 +1,22 @@
-import numpy
+import numpy as np
 
 def main():
-    x = numpy.loadtxt(open("housing_train.txt", "rb"), delimiter=" ", usecols = range(12))
+    ### input
+    x = np.loadtxt(open("housing_train.txt", "rb"), delimiter=" ", usecols = range(12))
     print x
-    y = numpy.loadtxt(open("housing_train.txt", "rb"), delimiter=" ", usecols = [13])
+    y = np.loadtxt(open("housing_train.txt", "rb"), delimiter=" ", usecols = [13])
     print y
+
+    ### compute optimal weight vector w
+
+    xTx = np.matmul(np.transpose(x), x)
+    xTx_inverse = np.linalg.inv(xTx)
+    xTy = np.matmul(np.transpose(x), y)
+
+    w = np.matmul(xTx_inverse, xTy)
+    print "W: "
+    print w
+
 
 if __name__ == '__main__':
     main()
