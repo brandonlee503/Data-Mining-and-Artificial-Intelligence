8083264d5e878ba8a8c108e6d7a03d718d2cc3e2
alxngyn <alexlnguyen@gmail.com>
2017-04-16 22:54:00 -0700

    started batch work

--- project2/main.py
+++ project2/main.py
@@ -2,10 +2,32 @@ import numpy as np
 
 
 
+
+# TODO: input,work, output
+
 def main():
     print "main"
 
 
+
+
+
+
+
+def compute_d(x,y,w):
+    y_hat = (1 / (1 + exp( -w * x )))
+    error = y - y_hat
+    d = d + error * x
+    return d
+
+def compute_w(x,y):
+    ### compute optimal weight vector w
+    xTx = np.matmul(np.transpose(x), x)
+    xTx_inverse = np.linalg.inv(xTx)
+    xTy = np.matmul(np.transpose(x), y)
+
+    w = np.matmul(xTx_inverse, xTy)
+    return w
 
 if __name__ == '__main__':
     main()
