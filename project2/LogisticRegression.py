1f09bf42baeec2166a645081bff8f20d89246ba1
John Miller <johnlukemiller@gmail.com>
2017-04-19 11:04:10 -0700

    Finishes problem 2 and moves logistic regression code into class

---
+++ project2/LogisticRegression.py
@@ -0,0 +1,27 @@
+import numpy as np
+
+class LR:
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def computePrediction(row, w):
+        return 1.0 / (1.0 + np.exp(-1.0 * (np.dot(w, row))))
+
+    def computeCoefficient(self, x, y, w):
+        d = np.zeros(w.shape[0])
+
+        for row in range(len(x)):
+            yHat = self.computePrediction(x[row], w)
+            error = float(y[row]) - yHat
+            d = np.add(d, np.multiply(error, x[row]))
+        return d
+
+    def testData(self, w, x, y):
+        correct = 0
+        for i, row in enumerate(x):
+            prediction = self.computePrediction(row, w)
+            if round(prediction) == y[i]:
+                correct = correct + 1
+
+        return float(correct) / float(y.shape[0])
--- project2/hw2.py
+++ project2/hw2.py
@@ -6,40 +6,41 @@ import numpy as np
 import csv
 from numpy import genfromtxt
 from math import exp
+from LogisticRegression import LR
 
 LEARNING_RATE = 0.000000001
-N_EPOCH = 10
+N_EPOCH = 900
 
 def main():
+    lr = LR()
+    print 'Loading data...'
     XTrain, YTrain, XTest, YTest = getData()
-    computeCoefficient(XTrain, YTrain, LEARNING_RATE, N_EPOCH)
+    print 'Starting problem 2...'
+    problem2(XTrain, YTrain, XTest, YTest, LEARNING_RATE, N_EPOCH, lr)
+
+def problem2(XTrain, YTrain, XTest, YTest, learningRate, nEpoch, lr):
+    w = np.zeros(XTrain.shape[1])
+    trainCorrectness = []
+    testCorrectness = []
+
+    print 'Training model and testing correctness...'
+    for epoch in range(nEpoch):
+        d = lr.computeCoefficient(XTrain, YTrain, w)
+        w = w + (learningRate * d)
+
+        trainCorrectness.append(lr.testData(w, XTrain, YTrain))
+        testCorrectness.append(lr.testData(w, XTest, YTest))
 
 def getData():
-    trainData = genfromtxt('usps-4-9-train.csv', delimiter=',')
-    testData = genfromtxt('usps-4-9-train.csv', delimiter=',')
+    training = genfromtxt('usps-4-9-train.csv', delimiter=',')
+    testing = genfromtxt('usps-4-9-test.csv', delimiter=',')
 
-    XTrain = np.array(trainData[:,:256])
-    YTrain = np.array(trainData[:,256:257])
-    XTest = np.array(testData[:,:256])
-    YTest = np.array(testData[:,256:257])
+    XTrain = np.array(training[:,:256])
+    YTrain = np.array(training[:,256:257])
+    XTest = np.array(testing[:,:256])
+    YTest = np.array(testing[:,256:257])
 
     return XTrain, YTrain, XTest, YTest
 
-# Logistic regression prediction
-def computePrediction(row, w):
-    return 1.0 / (1.0 + np.exp(-1.0 * (np.dot(w, row))))
-
-# Calculate coefficients w/ batch stochastic gradient descent
-def computeCoefficient(trainingX, trainingY, learningRate, nEpoch):
-    w = np.zeros(trainingX.shape[1])
-    for epoch in range(nEpoch):
-        d = np.zeros(trainingX.shape[1])
-        for row in range(len(trainingX)):
-            yHat = computePrediction(trainingX[row], w)
-            error = float(trainingY[row]) - yHat
-            d = np.add(d, np.multiply(error, trainingX[row]))
-        w = w + (learningRate*d)
-    return w
-    
 if __name__ == '__main__':
     main()
