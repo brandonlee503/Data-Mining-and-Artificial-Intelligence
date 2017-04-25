f8cb01bff381330531129698fedfb9a73f214da0
John Miller <johnlukemiller@gmail.com>
2017-04-25 16:23:13 -0700

    Adds function to read testing and training data

--- project3/hw3.py
+++ project3/hw3.py
@@ -1,5 +1,24 @@
+import sys
+import numpy as np
+import csv
+from numpy import genfromtxt
+from math import exp
+
 def main():
-    pass
+    XTrain, YTrain, XTest, YTest = getData()
+
+
+def getData():
+    training = genfromtxt('knn_train.csv', delimiter=',')
+    testing = genfromtxt('knn_test.csv', delimiter=',')
+
+    XTrain = np.array(training[:,1:31])
+    YTrain = np.array(training[:,0:1])
+    XTest = np.array(testing[:,1:31])
+    YTest = np.array(testing[:,0:1])
+
+    return XTrain, YTrain, XTest, YTest
+
 
 if __name__ == '__main__':
     main()
