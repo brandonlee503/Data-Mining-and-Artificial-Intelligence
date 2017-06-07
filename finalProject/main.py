1f18d92eef77d79b42e720e9e91dd8eda7d57923
John Miller <johnlukemiller@gmail.com>
2017-06-06 19:32:50 -0700

    Adds loading for testing data and removes unnecessary code

--- finalProject/main.py
+++ finalProject/main.py
@@ -1,27 +1,17 @@
-import sys
 import numpy as np
-from numpy import genfromtxt
-from StringIO import StringIO
 import pandas as pd
 
 def main():
     # Seperate data and class values
-    XTrain = getData()
+    train, test = getData()
 
 
 # Parse CSV
 def getData():
     training = pd.read_csv('train.csv', quotechar='"', skipinitialspace=True)
-    training = training.values
-    print(training[0])
-    # print(training[0])
-    # testing = genfromtxt('test.csv', delimiter=',')
-    # XTrain = np.array(training[:,0:5])
-    # print(XTrain)
-    # YTrain = np.array(training[:,5:6])
-    # XTest = np.array(testing[:,1:31])
-    # YTest = np.array(testing[:,0:1])
-    return training
+    testing = pd.read_csv('test.csv', quotechar='"', skipinitialspace=True)
+
+    return training.values, testing.values
 
 
 if __name__ == '__main__':
